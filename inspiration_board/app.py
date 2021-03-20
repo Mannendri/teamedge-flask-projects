a5811a883c3ebeba1b106dcf15d53144f763af3b
Mannendri Olivares <molivares2022@columbiasecondary.org>
2021-03-20 11:59:22 -0400

    Added a name field to form

--- inspiration_board/app.py
+++ inspiration_board/app.py
@@ -1,23 +1,26 @@
 from flask import Flask, request, render_template, current_app as app
-from sense_hat import SenseHat  
+from sense_emu import SenseHat  
 from time import sleep
 
 app = Flask(__name__)
 sense = SenseHat()
-messages = []
-@app.route('/', methods=['GET', 'POST'])
+messages = {}
+@app.route('/')
 def index():
+    return render_template('index.html')
+@app.route('/success', methods=['GET', 'POST'])
+def success():
     if request.method == "POST":
         message = request.form.get("message")
-        messages.append(message)
+        author = request.form.get("author")
+        messages[author] = message
         sense.show_message(message)
-    return render_template('index.html')
+    return render_template('success.html', message = message, author = author)
+
 
 @app.route('/all_messages')
 def all_messages():
-    for message in messages:
-        sense.show_message(message)
-    return render_template('all_messages.html')
+    return render_template('all_messages.html', messages = messages)
 
 
 if __name__ == '__main__':

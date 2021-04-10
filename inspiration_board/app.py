0f9c41a32ccfa1e4b6857249ce630b1c2c954baf
Mannendri Olivares <molivares2022@columbiasecondary.org>
2021-04-10 14:03:30 -0400

    Implemented sqlite database for storing messages

--- inspiration_board/app.py
+++ inspiration_board/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, render_template, current_app as app
-from sense_emu import SenseHat  
+from sense_hat import SenseHat  
 from time import sleep
+import sqlite3
 
 app = Flask(__name__)
 sense = SenseHat()
-messages = {}
+sense.low_light = True
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -14,12 +16,26 @@ def success():
     if request.method == "POST":
         message = request.form.get("message")
         author = request.form.get("author")
-        messages[author] = message
-        sense.show_message(message)
+
+        #connect to database and insert message and name
+        conn = sqlite3.connect("./static/data/messages.db")
+        curs = conn.cursor()
+        curs.execute("INSERT INTO messages VALUES((?),(?))", (author, message))
+        conn.commit()
+        conn.close()
+        sense.show_message(message)                                             
     return render_template('success.html', message = message, author = author)
 
 @app.route('/all_messages')
 def all_messages():
+    conn = sqlite3.connect("./static/data/messages.db")
+    curs = conn.cursor()
+    messages = []
+    rows = curs.execute("SELECT * FROM messages")
+    for row in rows:
+        message = {'name':row[0], 'message':row[1]}
+        messages.append(message)
+    conn.close()
     return render_template('all_messages.html', messages = messages)
 
 

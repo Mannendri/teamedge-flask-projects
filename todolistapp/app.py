b83f0b007ac6487aa2f5a169a2ef4a7cb9276e5e
Mannendri Olivares <molivares2022@columbiasecondary.org>
2021-04-24 14:41:00 -0400

    Started To-Do List App

---
+++ todolistapp/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from flask_apscheduler import APScheduler
+
+app = Flask (__name__)
+scheduler = APScheduler()
+scheduler.init_app(app)
+scheduler.start()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+    

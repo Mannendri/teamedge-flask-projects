10bc3839bf9210216c7e24062c4266a029772088
Mannendri Olivares <molivares2022@columbiasecondary.org>
2021-05-01 13:58:28 -0400

    Finished basic capability for adding tasks

--- todolistapp/app.py
+++ todolistapp/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_apscheduler import APScheduler
+import sqlite3
 
 app = Flask (__name__)
 scheduler = APScheduler()
@@ -8,9 +9,28 @@ scheduler.start()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    conn = sqlite3.connect("./static/data/tasks.db")
+    curs = conn.cursor()
+    tasks = []
+    rows = curs.execute("SELECT * FROM tasks")
+    for row in rows:
+        task = {'description':row[0], 'reminder':row[1]}
+        tasks.append(task)
+    conn.close() 
+    return render_template('index.html', tasks=tasks)
 
-
+@app.route('/todo', methods=['POST'])
+def todo():
+    if request.method == 'POST':
+        conn = sqlite3.connect("./static/data/tasks.db")
+        curs = conn.cursor()
+        task_name = request.form.get("description")
+        reminder = request.form.get("date")    
+        #connect to database and insert task
+        curs.execute("INSERT INTO tasks VALUES((?),(?))", (task_name, reminder))
+        conn.commit()        
+   
+    return redirect(url_for('index'))
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
     
